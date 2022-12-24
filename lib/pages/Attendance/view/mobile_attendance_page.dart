37c72e7d3411e749c0b966043454a5c82d498adc
thohid44 <thohidislam44@gmail.com>
2022-12-24 08:36:51 +0600

    24-12-2022

--- lib/pages/Attendance/view/mobile_attendance_page.dart
+++ lib/pages/Attendance/view/mobile_attendance_page.dart
@@ -17,41 +17,41 @@ class MobileAttendancePage extends StatefulWidget {
 }
 
 class _MobileAttendancePageState extends State<MobileAttendancePage> {
-
   bool status1 = false;
-   bool status2 = false;
-    bool status3 = false;
+  bool status2 = false;
+  bool status3 = false;
 
-    List<DropdownMenuItem<String>> get dropdownItems{
-  List<DropdownMenuItem<String>> menuItems = [
-  const  DropdownMenuItem(child: Text("Class Five"),value: "Academic Class"),
-  const  DropdownMenuItem(child: Text("Class Five"),value: "five"),
-   const DropdownMenuItem(child: Text("Class Six"),value: "six"),
-   const DropdownMenuItem(child: Text("Class Seven"),value: "seven"),
-   const DropdownMenuItem(child: Text("Class Eight"),value: "eight"),
-  ];
-  return menuItems;
-}
-String selectedValue = "Academic Class";
-
-  Future<MobileAttendFetchClass> fetchMobileCls() async{
-
- var response = await ApiUrl.userClient.post(Uri.parse("https://demo.webpointbd.com/api/mobile-attendance?class_id=1"), 
- headers: {    
-      'Accept':'application/json',
-      'Authorization': 'Bearer '+ApiUrl.token,
-    }
-
- );
-    var jsonData= json.decode(response.body);
-
-     if(response.statusCode==200){
-      return MobileAttendFetchClass.fromJson(jsonData);
-     }else{
-      return MobileAttendFetchClass.fromJson(jsonData);
-     }
-
+  List<DropdownMenuItem<String>> get dropdownItems {
+    List<DropdownMenuItem<String>> menuItems = [
+      const DropdownMenuItem(
+          child: Text("Select Class"), value: "Select Class"),
+      const DropdownMenuItem(child: Text("Class One"), value: "1"),
+      const DropdownMenuItem(child: Text("Class Two"), value: "2"),
+      const DropdownMenuItem(child: Text("Class Three"), value: "3"),
+      const DropdownMenuItem(child: Text("Class Four"), value: "4"),
+    ];
+    return menuItems;
   }
+
+  String selectedValue = "Select Class";
+  bool selectClass = false;
+  Future<MobileAttendFetchClass> fetchMobileCls() async {
+    var response = await ApiUrl.userClient.post(
+        Uri.parse(
+            "https://demo.webpointbd.com/api/mobile-attendance?class_id=$selectedValue"),
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer ' + ApiUrl.token,
+        });
+    var jsonData = json.decode(response.body);
+
+    if (response.statusCode == 200) {
+      return MobileAttendFetchClass.fromJson(jsonData);
+    } else {
+      return MobileAttendFetchClass.fromJson(jsonData);
+    }
+  }
+
   @override
   void initState() {
     fetchMobileCls();
@@ -59,85 +59,117 @@ String selectedValue = "Academic Class";
 
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:const Text("MOBILE ATTENDENCE"),
-        backgroundColor: primaryColor,
-      ),
-       body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListView(
-          children: [
-           
-  Container(
-    margin: EdgeInsets.symmetric(horizontal: 30.w, vertical: 10.h),
-            height: 45.h,
-            alignment: Alignment.center,
-            width: 150.w,
-            decoration: BoxDecoration(
-              border: Border.all(width: 1.w, color: Colors.grey)
-            ),
-             child: DropdownButton(
-              underline: SizedBox(),
-      value: selectedValue,
-      style: TextStyle(color: Colors.black,fontSize: 17.sp),
-      onChanged: (String? newValue){
-        setState(() {
-          selectedValue = newValue!;
-        });
-      },
-      items: dropdownItems
-      )
-           ),
-             
-           
-               SizedBox(height: 10.h,),
-               FutureBuilder(
-                future: fetchMobileCls(),
-                builder:(context, snapshot) {
-                  if(snapshot.hasData){
-                    return  DataTable(columns:const [
-              DataColumn(label:Text("ID",style: TextStyle(fontWeight: FontWeight.bold),) ),
-                DataColumn(label:Text("Name",style: TextStyle(fontWeight: FontWeight.bold),) ),
-                  DataColumn(label:Text("Status",style: TextStyle(fontWeight: FontWeight.bold),) )
-            ], rows: snapshot.data!.attendances!.map((e) =>DataRow(cells: [
-                DataCell(customText(e.studentId.toString(), dark, 13.0, FontWeight.bold),),
-                   DataCell(customText(e.studentName.toString(), dark, 13.0, FontWeight.bold),), 
-                   DataCell(Switch(
-                              
-                              value: status1, onChanged: (value){
-                                    setState(() {
-                                      status1 = value;
-                                    });
-                            }),)
-              ])).toList()
-              
-            );
-                  }
-                 return Center(child: CircularProgressIndicator(),);
-               },),
-            
-            SizedBox(height: 20.h,), 
-
-
-              Container(
-                margin: EdgeInsets.only(left: 30.w, right: 30.w),
-                alignment: Alignment.center,
-                height: 50.h, width: 200.w,
-
-
-                decoration: BoxDecoration(
-                  color: Colors.green, 
-                  
-                ),
-                child: customText("Save Attendance", Colors.white, 18.0, FontWeight.bold),
-              )
-           
-          ],
+        appBar: AppBar(
+          title: const Text("MOBILE ATTENDENCE"),
+          backgroundColor: primaryColor,
         ),
-      )
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(
+            children: [
+              Container(
+                  margin:
+                      EdgeInsets.symmetric(horizontal: 30.w, vertical: 10.h),
+                  height: 45.h,
+                  alignment: Alignment.center,
+                  width: 150.w,
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 1.w, color: Colors.grey)),
+                  child: DropdownButton(
+                      underline: SizedBox(),
+                      value: selectedValue,
+                      style: TextStyle(color: Colors.black, fontSize: 17.sp),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          selectedValue = newValue!;
+                          if (selectedValue == "Select Class") {
+                            selectClass = false;
+                          } else {
+                            selectClass = true;
+                          }
+                        });
+                      },
+                      items: dropdownItems)),
+              SizedBox(
+                height: 10.h,
+              ),
+              selectClass == true
+                  ? FutureBuilder(
+                      future: fetchMobileCls(),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return DataTable(
+                              columns: const [
+                                DataColumn(
+                                    label: Text(
+                                  "ID",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                )),
+                                DataColumn(
+                                    label: Text(
+                                  "Name",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                )),
+                                DataColumn(
+                                    label: Text(
+                                  "Status",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ))
+                              ],
+                              rows: snapshot.data!.attendances!
+                                  .map((e) => DataRow(cells: [
+                                        DataCell(
+                                          customText(e.studentId.toString(),
+                                              dark, 13.0, FontWeight.bold),
+                                        ),
+                                        DataCell(
+                                          customText(e.studentName.toString(),
+                                              dark, 13.0, FontWeight.bold),
+                                        ),
+                                        DataCell(
+                                          Switch(
+                                              value: status1,
+                                              onChanged: (value) {
+                                                setState(() {
+                                                  status1 = value;
+                                                });
+                                              }),
+                                        )
+                                      ]))
+                                  .toList());
+                        }
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      },
+                    )
+                  : Container(
+                      child: Center(
+                        child: Text("Please Select a Class"),
+                      ),
+                    ),
+              SizedBox(
+                height: 20.h,
+              ),
+              selectClass == true
+                  ? Container(
+                      margin: EdgeInsets.only(left: 30.w, right: 30.w),
+                      alignment: Alignment.center,
+                      height: 50.h,
+                      width: 200.w,
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                      ),
+                      child: customText("Save Attendance", Colors.white, 18.0,
+                          FontWeight.bold),
+                    )
+                  : Container()
+            ],
+          ),
+        ));
   }
 }
--- lib/pages/Attendance/view/student_wise_atten_report.dart
+++ lib/pages/Attendance/view/student_wise_atten_report.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 
-import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -19,30 +18,29 @@ class StudentWishAttendence extends StatefulWidget {
 }
 
 class _StudentWishAttendenceState extends State<StudentWishAttendence> {
+  Future<StudentWiseAttenModel> fetchStudent() async {
+    String url =
+        'https://demo.webpointbd.com/api/student-wise-attendance?studentId=S10575';
 
-  Future<StudentWiseAttenModel> fetchStudent() async{
-   
-   String url = 'https://demo.webpointbd.com/api/student-wise-attendance?studentId=S10575'; 
+    var response = await ApiUrl.userClient.get(Uri.parse(url), headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer' + ApiUrl.token,
+    });
 
-      var response = await ApiUrl.userClient.get(Uri.parse(url),
-headers: {    
-      'Accept':'application/json',
-      'Authorization': 'Bearer'+ApiUrl.token,
-    } );
-    
-     var data = json.decode(response.body);
-        print(data);
-    if(response.statusCode==200){
+    var data = json.decode(response.body);
+    print(data);
+    if (response.statusCode == 200) {
       return StudentWiseAttenModel.fromJson(data);
-    }else{
-      return StudentWiseAttenModel.fromJson(data); 
+    } else {
+      return StudentWiseAttenModel.fromJson(data);
     }
-     
-    }
-  void initState(){
-   fetchStudent(); 
+  }
+
+  final TextEditingController _stdId = TextEditingController();
+  void initState() {
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,159 +48,178 @@ headers: {
         backgroundColor: primaryColor,
         title: const Text("STUDENT WISE ATTENDENCE"),
       ),
-       body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: FutureBuilder(
-          future: fetchStudent(),
-          builder:((context, snapshot) {
-         
-          if(snapshot.connectionState ==ConnectionState.done){
-          return SingleChildScrollView(
-            child: Column(
-            children: [
-             Container(
-            
-              alignment: Alignment.center,
-              height: 40.h,
-              margin: EdgeInsets.symmetric(horizontal: 30.w, vertical:10.h),
-              child: TextFormField(
-                textAlign: TextAlign.center,
-                decoration:const InputDecoration(
-                  hintText:"STUDENT ID",
-                  enabledBorder: OutlineInputBorder(
-                    
-                  ), 
-                  focusedBorder: OutlineInputBorder(
-                    
-                  )
-                ),
-          
-              ),
-             ), 
-          
-              SizedBox(height: 10.h,),
-              Container(
-                padding: EdgeInsets.only(left: 10.w, bottom: 5.h),
-                alignment: Alignment.centerLeft,
-                child: customText("Student Id: ${ snapshot.data!.studentId.toString() }", dark, 16.0, FontWeight.bold)),
-               Container(
-                         padding: EdgeInsets.only(left: 10.w, bottom: 5.h),
-                alignment: Alignment.centerLeft,
-                child: customText("Name: ${ snapshot.data!.studentName.toString() }", dark, 16.0, FontWeight.bold)),
-                Container(
-                           padding: EdgeInsets.only(left: 10.w, bottom: 5.h),
-                alignment: Alignment.centerLeft,
-                  child: customText("Section: ${ snapshot.data!.section.toString() }", dark, 16.0, FontWeight.bold)),
-          
-          
-              SizedBox(height: 10.h,),
-               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-
-                  customText("P: ${ snapshot.data!.studentName.toString() }", Colors.green, 16.0, FontWeight.bold),
-                  
-                   customText("A:1", Colors.red, 16.0, FontWeight.bold),
-            customText("D:1", Colors.purple, 16.0, FontWeight.bold),
-             customText("L:1", Colors.lightBlue, 16.0, FontWeight.bold),
-              customText("H:1", Colors.yellow, 16.0, FontWeight.bold),
-              customText("LE:1",Colors.blue , 16.0, FontWeight.bold),
-              customText("E:1", Colors.redAccent, 16.0, FontWeight.bold),
-              customText("W:4", Colors.blueAccent, 16.0, FontWeight.bold),
-                ],
-               ), 
-                 SizedBox(height: 5.h,),
-          
-                  Container(
-            
-            height: 300.h,
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: DataTable(
-                columnSpacing: 10.w,
-                horizontalMargin: 10,
-                dataRowHeight: 20.h,
-                border: TableBorder.all(width: 1), 
-                columns:const[
-                    DataColumn(label: Text("ID")), 
-                  DataColumn(label: Text("Date")), 
-               
-                    DataColumn(label: Text("In")), 
-                    DataColumn(label: Text("Out")), 
-                    DataColumn(label: Text("Status")), 
-                ], 
-                
-                rows: snapshot.data!.attendances!.map((e) => DataRow(cells:[
-              DataCell(Text(e.id.toString())), 
-              DataCell(Text(e.date.toString())),
-                 DataCell(Text("10:10:10")),
-              DataCell(Text(e.inTime.toString())), 
-              // DataCell(Text(e.outTime.toString())), 
-              DataCell(Text(e.attnStatus.toString()))
-              
-                ])).toList()
-              ),
-            ),
-                  ), 
-                   
-          
-              SizedBox(height: 15.h,), 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  customText("P - Present", Colors.green, 13.0, FontWeight.bold),
-                   customText("A - Absent", Colors.red, 13.0, FontWeight.bold),
-            customText("D - Delay", Colors.purple, 13.0, FontWeight.bold),
-                customText("L - Leave", Colors.lightBlue, 13.0, FontWeight.bold),
-               
-                customText("H - Holiday", Colors.yellow, 16.0, FontWeight.bold),
-                ],
-               ), 
-            SizedBox(height: 5.h,), 
-               Row(
-                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                
-              customText("LE - Late & Early Leave",Colors.blue , 13.0, FontWeight.bold),
-              customText("E - Early Leave", Colors.redAccent, 13.0, FontWeight.bold),
-              customText("W - Weekly Off", Colors.blueAccent, 13.0, FontWeight.bold),
-                ],
-               )
-            ],
-                  ),
-          );
-          }
-          return Center(child: CircularProgressIndicator());
-        }))
-      ), 
-  bottomNavigationBar: Container(
-    
-    child: CurvedNavigationBar(
-      backgroundColor:Colors.white, 
-            key: _bottomNavigationKey,
-            index: 0,
-            height: 60.0,
-            items: <Widget>[
-              Icon(Icons.add, size: 30),
-              Icon(Icons.list, size: 30),
-              Icon(Icons.compare_arrows, size: 30),
-              Icon(Icons.call_split, size: 30),
-              Icon(Icons.perm_identity, size: 30),
-            ],
-            color: primaryColor,
-            buttonBackgroundColor: primaryColor,
-         
-            animationCurve: Curves.easeInOut,
-            animationDuration: Duration(milliseconds: 600),
-            onTap: (index) {
-              setState(() {
-                _page = index;
-              });
-            },
-            letIndexChange: (index) => true,
-          ),
-  ),
-     
+      body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: FutureBuilder(
+              future: fetchStudent(),
+              builder: ((context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Container(
+                          alignment: Alignment.center,
+                          height: 40.h,
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 30.w, vertical: 10.h),
+                          child: TextFormField(
+                            controller: _stdId,
+                            textAlign: TextAlign.center,
+                            decoration: const InputDecoration(
+                                hintText: "STUDENT ID",
+                                enabledBorder: OutlineInputBorder(),
+                                focusedBorder: OutlineInputBorder()),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10.h,
+                        ),
+                        Container(
+                            padding: EdgeInsets.only(left: 10.w, bottom: 5.h),
+                            alignment: Alignment.centerLeft,
+                            child: customText(
+                                "Student Id: ${snapshot.data!.studentId.toString()}",
+                                dark,
+                                16.0,
+                                FontWeight.bold)),
+                        Container(
+                            padding: EdgeInsets.only(left: 10.w, bottom: 5.h),
+                            alignment: Alignment.centerLeft,
+                            child: customText(
+                                "Name: ${snapshot.data!.studentName.toString()}",
+                                dark,
+                                16.0,
+                                FontWeight.bold)),
+                        Container(
+                            padding: EdgeInsets.only(left: 10.w, bottom: 5.h),
+                            alignment: Alignment.centerLeft,
+                            child: customText(
+                                "Section: ${snapshot.data!.section.toString()}",
+                                dark,
+                                16.0,
+                                FontWeight.bold)),
+                        SizedBox(
+                          height: 10.h,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            customText(
+                                "P: ${snapshot.data!.studentName.toString()}",
+                                Colors.green,
+                                16.0,
+                                FontWeight.bold),
+                            customText(
+                                "A:1", Colors.red, 16.0, FontWeight.bold),
+                            customText(
+                                "D:1", Colors.purple, 16.0, FontWeight.bold),
+                            customText(
+                                "L:1", Colors.lightBlue, 16.0, FontWeight.bold),
+                            customText(
+                                "H:1", Colors.yellow, 16.0, FontWeight.bold),
+                            customText(
+                                "LE:1", Colors.blue, 16.0, FontWeight.bold),
+                            customText(
+                                "E:1", Colors.redAccent, 16.0, FontWeight.bold),
+                            customText("W:4", Colors.blueAccent, 16.0,
+                                FontWeight.bold),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 5.h,
+                        ),
+                        Container(
+                          height: 300.h,
+                          child: SingleChildScrollView(
+                            scrollDirection: Axis.vertical,
+                            child: DataTable(
+                                columnSpacing: 10.w,
+                                horizontalMargin: 10,
+                                dataRowHeight: 20.h,
+                                border: TableBorder.all(width: 1),
+                                columns: const [
+                                  DataColumn(label: Text("ID")),
+                                  DataColumn(label: Text("Date")),
+                                  DataColumn(label: Text("In")),
+                                  DataColumn(label: Text("Out")),
+                                  // DataColumn(label: Text("Status")),
+                                ],
+                                rows: snapshot.data!.attendances!
+                                    .map((e) => DataRow(cells: [
+                                          DataCell(Text(e.id.toString())),
+                                          DataCell(Text(e.date.toString())),
+                                          DataCell(Text("10:10:10")),
+                                          DataCell(Text(e.inTime.toString())),
+                                          // DataCell(Text(e.outTime.toString())),
+                                          // DataCell(
+                                          //     Text(e.attnStatus.toString()))
+                                        ]))
+                                    .toList()),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 15.h,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            customText("P - Present", Colors.green, 13.0,
+                                FontWeight.bold),
+                            customText("A - Absent", Colors.red, 13.0,
+                                FontWeight.bold),
+                            customText("D - Delay", Colors.purple, 13.0,
+                                FontWeight.bold),
+                            customText("L - Leave", Colors.lightBlue, 13.0,
+                                FontWeight.bold),
+                            customText("H - Holiday", Colors.yellow, 16.0,
+                                FontWeight.bold),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 5.h,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            customText("LE - Late & Early Leave", Colors.blue,
+                                13.0, FontWeight.bold),
+                            customText("E - Early Leave", Colors.redAccent,
+                                13.0, FontWeight.bold),
+                            customText("W - Weekly Off", Colors.blueAccent,
+                                13.0, FontWeight.bold),
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                }
+                return Center(child: CircularProgressIndicator());
+              }))),
+      bottomNavigationBar: Container(
+        child: CurvedNavigationBar(
+          backgroundColor: Colors.white,
+          key: _bottomNavigationKey,
+          index: 0,
+          height: 60.0,
+          items: <Widget>[
+            Icon(Icons.add, size: 30),
+            Icon(Icons.list, size: 30),
+            Icon(Icons.compare_arrows, size: 30),
+            Icon(Icons.call_split, size: 30),
+            Icon(Icons.perm_identity, size: 30),
+          ],
+          color: primaryColor,
+          buttonBackgroundColor: primaryColor,
+          animationCurve: Curves.easeInOut,
+          animationDuration: Duration(milliseconds: 600),
+          onTap: (index) {
+            setState(() {
+              _page = index;
+            });
+          },
+          letIndexChange: (index) => true,
+        ),
+      ),
     );
   }
 
